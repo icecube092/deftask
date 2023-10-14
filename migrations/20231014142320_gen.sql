57ca7f54dff2af56c5018b858befc4398327d41b
kn <icecube092@gmail.com>
2023-10-14 15:45:59 +0200

    gen

--- migrations/20231014142320_gen.sql
+++ migrations/20231014142320_gen.sql
@@ -4,12 +4,27 @@
 -- generate rows
 INSERT INTO conn_log(addr)
 SELECT '0.0.0.0'::inet + i
-FROM generate_series(1, 1000000) as i;
+FROM generate_series(1, 500000) as i;
+
+INSERT INTO conn_log(addr)
+SELECT '0.0.0.0'::inet + i
+FROM generate_series(1, 500000) as i;
 
 -- create duplicates
 UPDATE conn_log
 SET addr = addr::inet - 500000
-WHERE addr::inet > '0.0.0.0'::inet+500000;
+WHERE addr::inet > '0.0.0.0'::inet + 500000;
+
+UPDATE conn_log
+SET addr = left(addr, length(addr) - 3);
+
+INSERT INTO conn_log
+SELECT *
+FROM conn_log;
+
+INSERT INTO conn_log
+SELECT *
+FROM conn_log;
 -- +goose StatementEnd
 
 -- +goose Down
